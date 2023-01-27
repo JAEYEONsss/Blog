da4140505969eca5e9c1f0c23af0055b445ce32f
JAEYEONsss <y718j@ajou.ac.kr>
2023-01-27 14:34:39 +0900

    chapter3 진행

---
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,38 @@
+package com.jojoldu.book.springboot.domain.posts;
+
+import com.jojoldu.book.springboot.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor// 기본 생성자 자동 추가 public Posts() {}와 같은 효과
+@Entity //table과 link될 클래스임을 나타낸다, 기본 생성자 필, Entity 클래스에는 Setter를 절대 만들지 않는다
+public class Posts extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
